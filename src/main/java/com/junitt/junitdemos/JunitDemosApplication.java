21947250bafd3bfa4778251544dc19ee8488c8ed
purushottamdeshmukh <deshmukh259.2010@gmail.com>
2023-10-18 10:08:56 +0530

    t1

---
+++ src/main/java/com/junitt/junitdemos/JunitDemosApplication.java
@@ -0,0 +1,13 @@
+package com.junitt.junitdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitDemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitDemosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junitt/junitdemos/JunitDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junitt.junitdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitDemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
